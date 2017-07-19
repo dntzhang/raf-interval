576c70b47ba639625d2fd0c0747f5b312b75b72d
dntzhang <kmdjs@qq.com>
2017-07-19 09:52:12 +0800

    。

---
+++ raf-interval.js
@@ -0,0 +1,48 @@
+/**
+ * Created by dntzhang on 2017/7/13.
+ */
+
+;(function() {
+
+    var queue = [],
+        id = -1,
+        ticking = false,
+        tickId = null
+
+    window.setRafInterval = function (fn, interval) {
+        id++
+        queue.push({id: id, fn: fn, interval: interval, lastTime: new Date()})
+        if (!ticking) {
+            var tick = function () {
+                tickId = requestAnimationFrame(tick)
+                queue.forEach(function (item) {
+                    if (item.interval < 17 || new Date - item.lastTime >= item.interval) {
+                        item.fn()
+                        item.lastTime = new Date()
+                    }
+                })
+            }
+            ticking = true
+            tick()
+        }
+        return id
+    }
+
+    window.clearRafInterval = function (id) {
+        var i = 0,
+            len = queue.length
+
+
+        for (; i < len; i++) {
+            if (id === queue[i].id) {
+                queue.splice(i, 1)
+                break
+            }
+        }
+
+        if (queue.length === 0) {
+            cancelAnimationFrame(tickId)
+            ticking = false
+        }
+    }
+})();
